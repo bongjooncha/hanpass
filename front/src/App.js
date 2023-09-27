0625edbc6c0070cdfcb84af3afc9c01a1e23c91d
hidden bebb <cbj4133@naver.com>
2023-09-28 02:49:39 +0900

    .

--- front/src/App.js
+++ front/src/App.js
@@ -3,16 +3,15 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-          Learn React
-      </header>
       <main>
         <navbar className = "BottomNav">
-          <a href="#">계좌관리</a>
-          <a href="#">내역관리</a>
-          <a href="#">이벤트</a>
-          <a href="#">고객센터</a>
-          <a href="#">설정</a>
+          <ul>
+            <li><a href="#"><img scr ="./botnavimg/계좌관리.JPG" alt="계좌관리"/></a></li>
+            <li><a href="#">내역관리</a></li>
+            <li><a href="#">이벤트</a></li>
+            <li><a href="#">고객센터</a></li>
+            <li><a href="#">설정</a></li>
+          </ul>
         </navbar>
       </main>
       <footer>
