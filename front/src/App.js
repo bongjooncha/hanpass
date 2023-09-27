d172daeb83f3d08ca22d2182a8f8421efed7d450
hidden bebb <cbj4133@naver.com>
2023-09-27 18:11:55 +0900

    Changes to be committed: 	modified:   front/src/App.css 	modified:   front/src/App.js

--- front/src/App.js
+++ front/src/App.js
@@ -8,11 +8,11 @@ function App() {
       </header>
       <main>
         <navbar className = "BottomNav">
-          <a href="">계좌관리</a>
-          <a>내역관리</a>
-          <a>이벤트</a>
-          <a>고객센터</a>
-          <a>설정</a>
+          <a href="#">계좌관리</a>
+          <a href="#">내역관리</a>
+          <a href="#">이벤트</a>
+          <a href="#">고객센터</a>
+          <a href="#">설정</a>
         </navbar>
       </main>
       <footer>
