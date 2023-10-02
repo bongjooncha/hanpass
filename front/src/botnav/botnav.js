ee0eab7d60b097a854d00c9a4909eea67d62ab98
hidden bebb <cbj4133@naver.com>
2023-10-02 16:09:39 +0900

    Changes to be committed: 	new file:   front/src/botnav/arrow.JPG 	modified:   front/src/botnav/botnav.css 	modified:   front/src/botnav/botnav.js 	new file:   front/src/botnav/page/accountcreate.JPG

--- front/src/botnav/botnav.js
+++ front/src/botnav/botnav.js
@@ -6,9 +6,13 @@ import m3 from "./m3.JPG";
 import m4 from "./m4.JPG";
 import m5 from "./m5.JPG";
 
+import arrow from "./arrow.JPG";
+
+import acccre from "./page/accountcreate.JPG"
+
 function Botnav(){
     return(
-        <div>
+        <div className ="BotNavDiv">
             <navbar className = "BottomNav">
                 <ul>
                 <li><a href="#"><img src ={m1} alt="계좌관리"/></a></li>
@@ -18,7 +22,33 @@ function Botnav(){
                 <li><a href="#"><img src ={m5} alt="설정"/></a></li>
                 </ul>
             </navbar>
+            <div id='tab'>
+                <div id="main">
+                    <img src={arrow}></img>
+                    <h3>계좌관리</h3>
+                </div>
+
+                <div id="accman">
+                    <img src={acccre}></img>
+                </div>
+
+                <div id="setting">
+                    <ul>
+                        <li>알림</li>
+                        <li>언어 (Language)</li>
+                        <li>로그인 비밀번호 변경</li>
+                        <li>6자리 비밀번호(PIN) 변경</li>
+                        <li>Face ID & 지문 인증관리</li>
+                        <li>휴대전화번호 변경</li>
+                        <li>약관</li>
+                        <li>버전정보</li>
+                        <li>회원탈퇴</li>
+                    </ul>
+                </div>
+
+            </div>
         </div>
+
     )
 }
 
