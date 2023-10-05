d02f4805f0ca577910beadce575d713cae7f8b95
HB <cbj4133@naver.com>
2023-10-05 14:56:58 +0900

    10-5

--- front/src/botnav/botnav.js
+++ front/src/botnav/botnav.js
@@ -32,6 +32,34 @@ function Botnav(){
                     <img src={acccre}></img>
                 </div>
 
+                <div id="achisman">
+                    <div id = "headachisman">
+                        <button>해외송금 보내기</button>
+                        <button>국내송금</button>
+                        <button>연락처 송금</button>
+                        <button>체크카드</button>
+                        <button>교통카드 충전</button>
+                        <button>결제</button>
+                        <button>ATM 출금</button>
+                        <button>국내 선불폰 충전</button>
+                    </div>
+                    <div id ="dateachisman">
+                        <button></button>
+                        <a>2023.10</a>
+                        <button></button>
+                    </div>
+                    <div id='memachisman'>
+                        <div id= 'time'>
+                            <a>최신순</a>
+                            <div id='line'></div>
+                            <a>과거순</a>
+                        </div>
+                        <a>
+                            내역이 없습니다.
+                        </a>
+                    </div>
+                </div>
+
                 <div id="setting">
                     <ul>
                         <li>알림</li>
