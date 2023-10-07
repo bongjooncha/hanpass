99dcc51e3e1860e2dd569030e75235c138a556d5
hidden bebb <cbj4133@naver.com>
2023-10-07 17:37:06 +0900

    Changes to be committed: 	modified:   front/src/botnav/botnav.css 	modified:   front/src/botnav/botnav.js 	new file:   front/src/botnav/page/accman/accman.js 	renamed:    front/src/botnav/page/accountcreate.JPG -> front/src/botnav/page/accman/accountcreate.JPG 	new file:   front/src/botnav/page/logman/logman.css 	new file:   front/src/botnav/page/logman/logman.js

--- front/src/botnav/botnav.js
+++ front/src/botnav/botnav.js
@@ -9,7 +9,8 @@ import m5 from "./m5.JPG";
 
 import arrow from "./arrow.JPG";
 
-import acccre from "./page/accountcreate.JPG"
+import Accman from './page/accman/accman.js'
+import Logman from './page/logman/logman.js'
 
 function Botnav(){
     const [headerText, setHeaderText]=useState('');
@@ -34,37 +35,10 @@ function Botnav(){
                     <h3>{headerText}</h3>
                 </div>
 
-                <div id="accman">
-                    <img src={acccre}></img>
-                </div>
+                <Accman/>
+                <Logman/>
 
-                <div id="achisman">
-                    <div id = "headachisman">
-                        <button>해외송금 보내기</button>
-                        <button>국내송금</button>
-                        <button>연락처 송금</button>
-                        <button>체크카드</button>
-                        <button>교통카드 충전</button>
-                        <button>결제</button>
-                        <button>ATM 출금</button>
-                        <button>국내 선불폰 충전</button>
-                    </div>
-                    <div id ="dateachisman">
-                        <button></button>
-                        <a>2023.10</a>
-                        <button></button>
-                    </div>
-                    <div id='memachisman'>
-                        <div id= 'time'>
-                            <a>최신순</a>
-                            <div id='line'></div>
-                            <a>과거순</a>
-                        </div>
-                        <a>
-                            내역이 없습니다.
-                        </a>
-                    </div>
-                </div>
+
 
                 <div id="setting">
                     <ul>
