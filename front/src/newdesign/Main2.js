79c5499e026d7d0b55885d78669228fb7a32c151
[AI9] 차봉준 <cbj0328@gmail.com>
2023-11-09 14:29:12 +0900

    img

--- front/src/newdesign/Main2.js
+++ front/src/newdesign/Main2.js
@@ -1,9 +1,15 @@
 import "./Main2.css";
 import React from "react";
 
+import Form from "react-bootstrap/Form";
+import Col from "react-bootstrap/Col";
+
+import arrow2 from "../png/arrow2.png";
+
 import bell from "../png/main_img/bell.png";
 import menu from "../png/main_img/menu.png";
 import mypage from "../png/main_img/mypage.png";
+import exchange from "../png/main_img/exchange.png";
 
 import ticket from "../png/main_img/ticket_1.png";
 
@@ -23,6 +29,12 @@ function Main({ setPoped }) {
 
   return (
     <>
+      <link
+        rel="stylesheet"
+        href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css"
+        integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM"
+        crossorigin="anonymous"
+      />
       <div className="Main2">
         <navbar className="TopNav2">
           <ul>
@@ -45,23 +57,41 @@ function Main({ setPoped }) {
           </ul>
         </navbar>
         <main>
-          <div id ="exchange">
-            <h4>KRW/USD = 1,312.00 </h4>
+          <div id="exchange">
+            <img src={exchange} />
+            {/* <div class="input-money">
+              <a>send</a>
+              <Form.Group as={Col} controlId="formGridState" id="row">
+                <Form.Select defaultValue="한국(KRW)" id="sel-coun">
+                  <option>한국(KRW)</option>
+                  <option>미국(USD)</option>
+                  <option>중국(CNY)</option>
+                  <option>일본(JPY)</option>
+                  <option>베트남(VND)</option>
+                  <option>태국(TBH)</option>
+                </Form.Select>
+                <input type="text" value="1,000,000" />
+              </Form.Group>
+            </div>
+            <img src={arrow2} />
+            <div class="output-money">
+              <a>get</a>
+              <Form.Group as={Col} controlId="formGridState" id="row">
+                <Form.Select defaultValue="미국(USD)" id="sel-coun">
+                  <option>미국(USD)</option>
+                  <option>한국(KRW)</option>
+                  <option>중국(CNY)</option>
+                  <option>일본(JPY)</option>
+                  <option>베트남(VND)</option>
+                  <option>태국(TBH)</option>
+                </Form.Select>
+                <input type="text" value="765" />
+              </Form.Group>
+            </div> */}
           </div>
-          <img id="wallet" src={wallet}/>
+          <img id="wallet" src={wallet} />
           <div>
             <img id="line4" src={line4} />
-          </div>
-          <div id = "community">
-            <div id = "selec_commu">
-              <div id="coun_commu">
-                <h4>한국 인기 글</h4>
-                <a>김패스</a><a>11/30 20:22</a>
-                <p>한패스 송금수수료 실화??</p>
-                <p>한패스 송금수수료 너무 쌈;; 말이안됨</p>
-                <a>한국 계시판 <img src="./mainimg/forthline.JPG"/></a>
-              </div>
-            </div>
           </div>
         </main>
         <Botnav />
